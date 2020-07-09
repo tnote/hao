9cffbb0e44ad2eebde64ece113384d34438094e6
okerivy <okerivy@163.com>
2020-07-09 12:07:14 +0800

    添加云盘精灵

--- static/js/keyword.js
+++ static/js/keyword.js
@@ -21,6 +21,7 @@ $(function () {
     { url: 'http://www.cilimao.cc/search?word=', type: 'cili' },
     { url: 'http://neets.cc/search?key=', type: 'yingyin' },
     { url: 'http://www.panduoduo.net/s/name/', type: 'wangpan' },
+    { url: 'https://www.yunpanjingling.com/search/', type: 'yunpan' },
   ];
   $searchLogo.on('click', function () {
     $searchMethods.show();
